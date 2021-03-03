66d4a5732e27b2aa5b27f40bd180d8edf2eadde8
eunjin kim <eunjin@ninewatt.com>
2021-03-03 09:40:03 +0900

    Java RoomDB : 모델(JavaTodo)를 활용하기 위한 CRUD

--- app/src/main/java/com/example/room_kotlin/JavaTodoDao.java
+++ app/src/main/java/com/example/room_kotlin/JavaTodoDao.java
@@ -1,4 +1,24 @@
 package com.example.room_kotlin;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
 public interface JavaTodoDao {
+    @Query("SELECT * FROM JavaTodo")
+    List<JavaTodo> getAll();
+
+    @Insert
+    void insert(JavaTodo javaTodo);
+
+    @Update
+    void update(JavaTodo javaTodo);
+
+    @Delete
+    void delete(JavaTodo javaTodo);
 }
