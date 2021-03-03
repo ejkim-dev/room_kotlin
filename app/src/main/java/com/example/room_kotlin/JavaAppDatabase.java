3bd9f03b596ad2e1d199556a431d541f97c8c2ad
eunjin kim <eunjin@ninewatt.com>
2021-03-03 09:50:00 +0900

    Java RoomDB : DB를 조작할 기본 준비 완료

--- app/src/main/java/com/example/room_kotlin/JavaAppDatabase.java
+++ app/src/main/java/com/example/room_kotlin/JavaAppDatabase.java
@@ -7,6 +7,7 @@ import androidx.room.InvalidationTracker;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteOpenHelper;
 
+/*JavaAppDatabase를 생성한 다음 todoDao를 통해서 JavaTodo를 조작*/
 @Database(entities = {JavaTodo.class}, version = 1)
 public abstract class JavaAppDatabase extends RoomDatabase {
   public abstract JavaTodoDao todoDao();
