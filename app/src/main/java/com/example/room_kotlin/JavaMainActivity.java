51884b0e4bac22b6655819349e2b89e9aeec2094
Eunjin <eunjin@ninewatt.com>
2021-03-09 13:45:09 +0900

    Java RoomDB : DB 객체 만들기, 'java-todo-db' 파일 생성

--- app/src/main/java/com/example/room_kotlin/JavaMainActivity.java
+++ app/src/main/java/com/example/room_kotlin/JavaMainActivity.java
@@ -2,6 +2,7 @@ package com.example.room_kotlin;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
 import android.os.Bundle;
 
@@ -12,6 +13,6 @@ public class JavaMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java_main);
 
-
+        JavaAppDatabase javaAppDatabase = Room.databaseBuilder(this, JavaAppDatabase.class, "java-todo-db").build();
     }
 }
