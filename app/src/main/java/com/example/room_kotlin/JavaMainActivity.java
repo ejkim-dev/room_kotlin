7a42142d021092ce3a3c88b1c13b408da9fd468e
Eunjin <eunjin@ninewatt.com>
2021-03-09 13:47:36 +0900

    Java RoomDB : 데이터 표시 코드 작성, 레이아웃 가져오기

--- app/src/main/java/com/example/room_kotlin/JavaMainActivity.java
+++ app/src/main/java/com/example/room_kotlin/JavaMainActivity.java
@@ -5,14 +5,22 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class JavaMainActivity extends AppCompatActivity {
+    private EditText mTodoEditText;
+    private TextView mResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java_main);
 
+        mTodoEditText = findViewById(R.id.todo_edit);
+        mResultTextView = findViewById(R.id.result_text);
+
         JavaAppDatabase javaAppDatabase = Room.databaseBuilder(this, JavaAppDatabase.class, "java-todo-db").build();
+
     }
 }
